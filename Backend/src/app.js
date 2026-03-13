6c9b9594a68ec69cad80ad64a726a1074d8edeba
Dakshinamoorthy-007 <dakshinamoorthy771@gmail.com>
2026-03-13 15:12:57 +0530

    Fix CORS configuration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,25 +6,23 @@ import batchRoutes from "./routes/batch.routes.js";
 import traceRoutes from "./routes/trace.routes.js";
 import adminRoutes from "./routes/admin.routes.js";
 
-
 const app = express();
+
+// 🔥 GLOBAL MIDDLEWARE
+app.use(cors({
+  origin: "https://agri-block-trace-nd6r.vercel.app"
+}));
+app.use(express.json());
+
+// Health check
 app.get("/", (req, res) => {
   res.json({ status: "AgriChain backend running" });
 });
-// 🔥 GLOBAL MIDDLEWARE
-app.use(cors());
-app.use(express.json());
-const cors = require("cors");
-
-app.use(cors({
-  origin: "https://agri-block-trace.vercel.app/"
-}));
 
 // 🔥 ROUTES
 app.use("/api/admin", adminRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/trace", traceRoutes);
 app.use("/api/batch", batchRoutes);
-
 
 export default app;
