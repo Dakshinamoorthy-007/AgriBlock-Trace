dce1136451e5fc101d9b486fc67215b5ebae8e3e
Dakshinamoorthy-007 <dakshinamoorthy771@gmail.com>
2026-03-13 15:07:38 +0530

    Connecting Frontend To Backend For Vercel

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -2,11 +2,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDCNgKliki6gBcM8tih4LAj21xdWQWO9HI",
   authDomain: "agrichain-fd6eb.firebaseapp.com",
